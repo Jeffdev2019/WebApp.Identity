bd315ed6cdbd75039797ab78fb7aef34a98ad7a5
jefferson victor <jeffersonvic2@outlook.com>
2020-04-24 07:17:40 -0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApp.Identity.Models;
+
+namespace WebApp.Identity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UserManager<MyUser> _userManager;
+
+        public HomeController(UserManager<MyUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        /*private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }*/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(model.UserName);
+
+                if (user != null && await _userManager.CheckPasswordAsync(
+                    user, model.Password))
+                {
+                    var identity = new ClaimsIdentity("coockies");
+                    identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.Id));
+                    identity.AddClaim(new Claim(ClaimTypes.Name, user.UserName));
+
+                    await HttpContext.SignInAsync("cookies", new ClaimsPrincipal(identity));
+
+                    return RedirectToAction("About");
+                }
+                ModelState.AddModelError("", "Usuário ou Senha invalida");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(model.UserName);
+
+                if (user == null)
+                {
+                    user = new MyUser()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        UserName = model.UserName,
+                        PasswordHash = model.Password
+                    };
+
+                    var result = await _userManager.CreateAsync(
+                        user, model.Password);
+                }
+                return View("Success");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# WebApp.Identity
+ Curso: Asp.NET Core Identity + MVC e Web API + SQL Server + EF Core
